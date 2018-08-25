9e77d1cdb05cc2cfad43c4b80284f3d3e0e79b65
Ismayatikiran <ismaytaikiran96@gmail.com>
2018-08-25 17:10:23 -0400

    My2nd

---
+++ src/finalize/example/TestFinalize.java
@@ -0,0 +1,16 @@
+package finalize.example;
+
+public class TestFinalize {
+
+    public static void main(String[]args) {
+        TestFinalize tf = new TestFinalize();
+    //    tf.finalize();
+        String st = new String("House");
+        System.out.println(st);
+     //   System.out.println(gc);
+        System.out.println("End of main");
+        System.out.println(st);
+
+    }
+}
+
